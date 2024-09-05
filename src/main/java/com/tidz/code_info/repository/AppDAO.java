407a6df58eaf510faffae17003d5bce146b03f26
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-05 10:56:33 -0300

    create AppDAO and its implementation

---
+++ src/main/java/com/tidz/code_info/repository/AppDAO.java
@@ -0,0 +1,9 @@
+package com.tidz.code_info.repository;
+
+import com.tidz.code_info.model.Programmer;
+
+public interface AppDAO {
+
+	void save(Programmer programmer);
+
+}
---
+++ src/main/java/com/tidz/code_info/repository/AppDAOImpl.java
@@ -0,0 +1,27 @@
+package com.tidz.code_info.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.tidz.code_info.model.Programmer;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+	private EntityManager entityManager;
+
+	@Autowired
+	public AppDAOImpl(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	@Transactional
+	public void save(Programmer programmer) {
+		this.entityManager.persist(programmer);
+	}
+
+}
