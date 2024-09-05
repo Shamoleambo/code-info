90e34ff5b0ca8309cae3bd0dbeabec2e64058c19
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-09-05 10:19:28 -0300

    first commit

---
+++ src/test/java/com/tidz/code_info/CodeInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.code_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
